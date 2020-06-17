ed4e7165def263d7e2b83244d7807e13ba3af629
Robert Gils <robertgils@gmail.com>
2020-06-17 20:33:31 +0200

    arquillian tests working

--- BDExchangeBackend/src/test/java/backend/resource/UsersResourceIT.java
+++ BDExchangeBackend/src/test/java/backend/resource/UsersResourceIT.java
@@ -1,23 +1,28 @@
 package backend.resource;
 
 import backend.App;
+import backend.dao.UserDao;
 import backend.domain.User;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
+import java.io.File;
 import java.net.URL;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(Arquillian.class)
 public class UsersResourceIT {
@@ -37,13 +42,37 @@ public class UsersResourceIT {
         WebArchive archive = ShrinkWrap.create(WebArchive.class, "test.war")
                 .addClass(App.class)
                 .addClass(UsersResource.class)
-                .addPackage(User.class.getPackage());
+                .addClass(UserDao.class)
+                .addPackage(User.class.getPackage())
+                .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
+                .addAsResource("test-beans.xml", "META-INF/beans.xml")
+                .addAsLibraries(hibernate());
         System.out.println(archive.toString(true));
         return archive;
     }
 
+    private static File[] hibernate() {
+        return Maven.resolver()
+                .loadPomFromFile("pom.xml")
+                .resolve("org.hibernate:hibernate-entitymanager")
+                .withTransitivity()
+                .asFile();
+    }
+
     @Test
     public void testUsersEndpointGetAll() {
+        System.out.println("UsersResource = " + usersResource);
+        String message = ClientBuilder.newClient()
+                .target(usersResource)
+                .request(MediaType.APPLICATION_JSON)
+                .get(String.class);
+
+        System.out.println(message);
+        assertThat(message, containsString("[{\"id\":"));
+    }
+
+    @Test
+    public void testUsersEndpointGetAll2() {
         System.out.println("UsersResource = " + usersResource);
         String message = ClientBuilder.newClient()
                 .target(usersResource)
